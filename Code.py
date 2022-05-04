abe97ca17873fab16a464f74f8d84d03fdff350e
Megha Shah <megha-paresh.shah@etu.u-paris.fr>
2022-05-04 14:13:40 +0200

    my third commit

--- Code.py
+++ Code.py
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 # Uploading the selected data
-import pandas as pd 
-lexi = pd.read_csv('lexique.csv')
-print(lexi)
-=======
-# Uploading the selected database
-import pandas as pd
-lexi = pd.read_csv('lexique.csv')
-print(lexi)
->>>>>>> eeb7c092ff876af23e59d3ad8f34e0f7dfb9a9af
+import pandas
+lexique = pandas.read_csv('lexique383.tsv.gz', sep='\t')
+words= list(lexique.ortho)
+print(words)
+
+# 
