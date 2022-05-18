af358a5fc302e7d855dee66f1d08c853285b9cc7
Megha Shah <megha-paresh.shah@etu.u-paris.fr>
2022-05-18 12:23:39 +0200

    my fifth commit

--- Code.py
+++ Code.py
@@ -2,31 +2,44 @@
 import pandas
 import random
 import string
-from collections import Counter
-
-
+import re
 # Uploading the selected data
 lexique = pandas.read_csv('lexique383.tsv.gz', sep='\t')
 words= list(lexique.ortho)
 print(words)
+words2 = [str(w) for w in words]
+# Extracting bi-grams and tri-grams
+def extract_bigrams(token):
+    return [token[i:i+2] for i in range(len(token) - 1)]
 
-# Generating random-stringe of segments
-def random_string(str_size, allowed_chars):
-    return ''.join(random.choice(allowed_chars) for x in range(str_size))
-chars = words
-size = 3
-print(chars)
-random_string = (random_string(size, chars))
-print(random_string)
+def extract_trigrams(token):
+    return [token[i:i+3] for i in range(len(token) - 1)]
+# Using the data to create bi-grams and tri-grams
+all_bi_grams = []
+for w in words2:
+    all_bi_grams.extend(extract_bigrams(w))
+all_tri_grams = []
+for w in words2:
+    all_tri_grams.extend(extract_trigrams(w))
+# Generating pseudowords
+pwb6 = ["".join(random.sample(all_bi_grams, 3)) for _ in range(10000)]
+pattern_bi = re.compile("^([ptkbdgcqwjfshlxvnmlrz][aeuio]|[aeuio][ptkbdgcqwjfshlxvnmlrz]|[aeuio][aeuio]|[ptkbdgcqwjfshlxvnmlrz][ptkbdgcqwjfshlxvnmlrz])+$")
+list_bi =[w for w in pwb6 if pattern_bi.match(w) is not None]
+pwt6 = ["".join(random.sample(all_tri_grams, 2)) for _ in range(10000)]
+pattern_tri = re.compile("^([ptkbdgcqwjfshlxvnmlrz][aeuio]|[aeuio][ptkbdgcqwjfshlxvnmlrz]|[aeuio][aeuio]|[ptkbdgcqwjfshlxvnmlrz][ptkbdgcqwjfshlxvnmlrz])+$")
+list_tri = [w for w in pwt6 if pattern_tri.match(w) is not None]
+print(list_tri)
+print(list_bi)
+# Phonological features and phonological rule
+for i in range(len(list_tri)):
+    if ["s"] in list_tri:
+        ["s"] += ["z"]
+print(list_tri)
+for i in range(len(list_bi)):
+    if ["s"] in list_bi:
+        ["s"] += ["z"]
+print(list_bi)
 
-# Creating bi-grams and tri-grams
-bigrams = Counter(zip(random_string, random_string[1:]))
-bi_grams = [print(bigrams)]
-trigrams = Counter(zip(random_string, random_string[1:]))
-tri_grams = [print(trigrams)]
-
-# Generating Pseudowords
-pseudowords = [RandomWords((bigrams)), RandomWords((trigrams))]
-print(pseudowords)
-# Phonological rule selection
-# Out-put pseudowords
+# Printing Pseudowords
+print(list_tri)
+print(list_bi)
